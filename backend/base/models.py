ab294014814b58aa80737ca4d04bace73f90a267
Abhishek Srivastava <abhisheksrivastavabbn@gmail.com>
2022-02-06 13:41:05 +0530

    Django backend

---
+++ backend/base/models.py
@@ -0,0 +1,12 @@
+from statistics import mode
+from tkinter.font import BOLD
+from django.db import models
+from django.contrib.auth.models import User
+
+
+# Create your models here.
+
+class Note(models.Model):
+
+  user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+  body = models.TextField()
